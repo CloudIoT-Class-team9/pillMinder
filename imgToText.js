f5924f25f9bfb4e6aa5a4cd4d00a62a67719aae4
parkhyeonji <iamphj3@naver.com>
2023-05-10 17:22:28 +0900

    test : tesseract 예제 테스트 성공

--- imgToText.js
+++ imgToText.js
@@ -1,1 +1,25 @@
-const Tesseract = require('tesseract.js');
+import { createWorker } from 'tesseract.js';
+
+const worker = await createWorker({
+  logger: (m) => console.log(m),
+});
+
+(async () => {
+  await worker.loadLanguage('eng');
+  await worker.initialize('eng');
+  const {
+    data: { text },
+  } = await worker.recognize('https://tesseract.projectnaptha.com/img/eng_bw.png');
+  console.log(text);
+  await worker.terminate();
+})();
+
+// const recognize = async ({ target: { files }  }) => {
+//     const { data: { text } } = await Tesseract.recognize(files[0], 'kor', {
+//         corePath: 'https://unpkg.com/tesseract.js-core@v2.0.0/tesseract-core.wasm.js',
+//         logger: m => console.log(m),
+//     });
+//     console.log(text);
+//   }
+//   const elm = document.getElementById('uploader');
+//   elm.addEventListener('change', recognize);
