b5bd951c82f0bba94d686d770da5493f4f6a47d8
parkhyeonji <iamphj3@naver.com>
2023-05-10 17:45:12 +0900

    test : tesseract 이미지 한글 추출 테스트

--- imgToText.js
+++ imgToText.js
@@ -5,14 +5,19 @@ const worker = await createWorker({
 });
 
 (async () => {
-  await worker.loadLanguage('eng');
-  await worker.initialize('eng');
+  await worker.loadLanguage('kor');
+  await worker.initialize('kor');
   const {
     data: { text },
-  } = await worker.recognize('https://tesseract.projectnaptha.com/img/eng_bw.png');
+  } = await worker.recognize('https://hkn24.com/news/photo/201811/302176_153811_4549.jpg');
   console.log(text);
   await worker.terminate();
 })();
+
+// TODO
+// 1. 이미지에서 한글 텍스트 추출 (완료)
+// 2. 제품명 정확하게 반환하도록 추출된 텍스트 가공
+// 3. 다양한 이미지로 테스트
 
 // const recognize = async ({ target: { files }  }) => {
 //     const { data: { text } } = await Tesseract.recognize(files[0], 'kor', {
