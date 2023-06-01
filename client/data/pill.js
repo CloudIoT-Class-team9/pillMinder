8d36bf3bbcadacd84e55473a3cf3166bb1f2f809
parkhyeonji <iamphj3@naver.com>
2023-06-01 18:32:30 +0900

    chore : 유저, 약 더미데이터 생성

---
+++ client/data/pill.js
@@ -0,0 +1,16 @@
+// 약 정보 임시 더미데이터
+
+export const PILL_REQ = {
+  pillname: '훼스탈골드정',
+};
+
+export const PILL_RES = {
+  effects: '두통',
+  adultUsage: '성인 1회 1정',
+  childUsage: 'null',
+  frequency: 'null',
+  alarm: {
+    adult: 3,
+    child: 1,
+  },
+};
---
+++ client/data/user.js
@@ -0,0 +1,13 @@
+// 사용자 fitbit 정보 임시 더미데이터
+
+export const FITBIT_REQ = {
+  clientId: 'Seung123',
+  clientPwd: 'test123',
+  Date: '2023 - 05 - 18',
+};
+
+export const FITBIT_RES = {
+  age: 24,
+  heartRate: '정상',
+  temperature: '36.4',
+};
